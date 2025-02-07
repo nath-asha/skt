f0b104bd4151b96991abcd38e80457fbe677da8f
nath-asha <omthasha@gmail.com>
2025-02-07 12:24:02 +0530

    added calc folder

---
+++ calculator/cal.js
@@ -0,0 +1,9 @@
+let a = prompt()
+let b = prompt()
+
+let add = (a,b) => { a+b }
+let substract = (a,b) => { a-b }
+let multiply = (a,b) => {a*b}
+let divde = (a,b) => {a/b}
+let modulo = (a,b) => {a%b}
+
