1074985f6c0111da13f86675ff74ee66d67ae018
nath-asha <omthasha@gmail.com>
2025-02-07 12:55:44 +0530

    changes

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,13 +1,58 @@
-let display = (a) =>{
-    print(a)
+function display(a){
+    document.getElementById('result').value += value
+    return val
 }
 
-// let a = prompt()
-// let b = prompt()
+function myFunction(event) { 
+    if (event.key == '0' || event.key == '1' 
+        || event.key == '2' || event.key == '3' 
+        || event.key == '4' || event.key == '5' 
+        || event.key == '6' || event.key == '7' 
+        || event.key == '8' || event.key == '9' 
+        || event.key == '+' || event.key == '-' 
+        || event.key == '*' || event.key == '/') 
+        document.getElementById("result").value += event.key; 
+} 
 
-// let add = (a,b) => { a+b }
-// let substract = (a,b) => { a-b }
-// let multiply = (a,b) => {a*b}
-// let divde = (a,b) => {a/b}
-// let modulo = (a,b) => {a%b}
+function solve(){
+    let x = document.getElementById('result').value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
+
+function clr()
+{
+    document.getElementById("result").value = ""
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let a = prompt()
+let b = prompt()
+
+let add = (a,b) => { a+b }
+let substract = (a,b) => { a-b }
+let multiply = (a,b) => {a*b}
+let divde = (a,b) => {a/b}
+let modulo = (a,b) => {a%b}
 
