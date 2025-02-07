1091a913afae1a58c1479cb19eb13260ba80c956
nath-asha <omthasha@gmail.com>
2025-02-07 12:41:01 +0530

    added calc

--- calculator/cal.js
+++ calculator/cal.js
@@ -1,9 +1,13 @@
-let a = prompt()
-let b = prompt()
+let display = (a) =>{
+    print(a)
+}
 
-let add = (a,b) => { a+b }
-let substract = (a,b) => { a-b }
-let multiply = (a,b) => {a*b}
-let divde = (a,b) => {a/b}
-let modulo = (a,b) => {a%b}
+// let a = prompt()
+// let b = prompt()
+
+// let add = (a,b) => { a+b }
+// let substract = (a,b) => { a-b }
+// let multiply = (a,b) => {a*b}
+// let divde = (a,b) => {a/b}
+// let modulo = (a,b) => {a%b}
 
